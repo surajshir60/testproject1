e160c6153ff2c99c7078904221759bccd4479e2f
surajshir60 <itzsurajshirsath@gmail.com>
2023-04-25 11:15:41 -1200

    Create testgitproject2

---
+++ src/test/java/testpackage/WelcomeTest.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class WelcomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome Git Hub");
+	}
+
+}
